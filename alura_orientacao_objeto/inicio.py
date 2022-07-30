d6759eff946eda066b576b01e7669b5e8ea2a19a
Giovanni <gioabrami@gmail.com>
2022-07-29 21:07:28 -0300

    exercicios feitos hoje

--- alura_orientacao_objeto/inicio.py
+++ alura_orientacao_objeto/inicio.py
@@ -1,17 +1,5 @@
 
-def cria_conta (conta, titular, saldo, limite):
-    conta = {"numero": conta, "titular": titular, "saldo": saldo, "limite": limite}
-    return conta
+from conta import Conta
 
-def deposito (conta, valor):
-    conta["saldo"] += valor
-
-def saque (conta, valor):
-    conta["saldo"] -= valor
-
-def extrato (conta):
-    print ("O saldo disponível é de R$ {}.".format (conta["saldo"]))
-
-conta = cria_conta (123, "Joao", 50.0, 1000.0)
-
-extrato (conta)
+conta1 = Conta (123, "Joao", 50.0, 1000.0)
+conta2 = Conta (321, "Marco", 100.0, 1000.0)

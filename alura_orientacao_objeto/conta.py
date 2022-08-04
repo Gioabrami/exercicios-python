97461b8a4e82758d77d112b3c6547aa68e1d3d97
Giovanni <gioabrami@gmail.com>
2022-08-04 11:16:55 -0300

    Exercícios finais lista estrutura de decisão e exercícios alura OO

--- alura_orientacao_objeto/conta.py
+++ alura_orientacao_objeto/conta.py
@@ -2,16 +2,34 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     
     def extrato (self):
-        print (f"Saldo de {self.saldo} do titular {self.titular}.")
+        print (f"Saldo de {self.__saldo} do titular {self.__titular}.")
 
     def deposita (self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca (self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere (self, valor, destino):
+        self.saca (valor)
+        destino.deposita (valor)
+
+    def get_saldo (self):
+        return self.__saldo
+
+    def get_titular (self):
+        return self.__titular
+
+    def get_limite (self):
+        return self.__limite
+
+    def set_limite (self, valor):
+        self.__limite = valor
+
+

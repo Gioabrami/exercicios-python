61a814a47e6ea76f34ae2e8840b99c3aa5934996
Giovanni <gioabrami@gmail.com>
2022-08-05 21:24:05 -0300

    Exercícios finalização do curso de Orientação a Objeto e início do curso de Avanço com Orientação a Objeto

--- alura_orientacao_objeto/conta.py
+++ alura_orientacao_objeto/conta.py
@@ -6,6 +6,7 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo_banco = "001"
     
     def extrato (self):
         print (f"Saldo de {self.__saldo} do titular {self.__titular}.")
@@ -13,23 +14,36 @@ class Conta:
     def deposita (self, valor):
         self.__saldo += valor
 
+    def __pode_sacar (self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
+
     def saca (self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar (valor):
+            self.__saldo -= valor
+        else:
+            print (f"O valor {valor} passou do limite.")
 
     def transfere (self, valor, destino):
         self.saca (valor)
         destino.deposita (valor)
 
-    def get_saldo (self):
+    @property
+    def saldo (self):
         return self.__saldo
 
-    def get_titular (self):
+    @property
+    def titular (self):
         return self.__titular
 
-    def get_limite (self):
+    @property
+    def limite (self):
         return self.__limite
 
-    def set_limite (self, valor):
+    @limite.setter
+    def limite (self, valor):
         self.__limite = valor
-
-
+    
+    @staticmethod
+    def codigo_banco ():
+        return "001"

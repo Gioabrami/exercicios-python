75c39f8b9256c7cb85c75e94e6bcfadc6bcd39a6
Giovanni <gioabrami@gmail.com>
2022-08-23 10:55:02 -0300

    Exercicio 23 finalizado

--- lista_estrutura_de_repeticao/exercicio23.py
+++ lista_estrutura_de_repeticao/exercicio23.py
@@ -4,22 +4,47 @@
 # (divisões) executados.
 
 maximo = int (input ('Digite o número limite para os testes: '))
-primos = [2]
+lista_primos = [2]
 divisoes = 0
 
-for num in range (2, maximo + 1):   #pega cada numero que vai ser testado
-    for posicao in primos:            #tenta dividir o número pelos primos que eu já tenho
-        resultado = num % primos [posicao - 1]
-        print ('Teste da divisão', num, '/', primos [posicao - 1])
-        divisoes += 1               #conta a divisão
-        if resultado == 0:        #se a divisão for exata, não é primo e não precisa continuar tentando
+def divisivel_pelos_primos (numero_para_testar, lista_primos, divisoes):
+    for num in lista_primos:
+        divisoes += 1
+        if numero_para_testar % num == 0:
+            nao_primo = True
             break
-        elif num % primo > 0:
-            for n in range (primos [-1], (num | 2) + 1, 2): #continua tentando do último primo até metade do número, pulando os pares
-                divisoes += 1
-                if num % n == 0:
-                    break
-                else:
-                    primos.append (num)
+        else:
+            nao_primo = False
+    
+    return nao_primo, divisoes
 
-print (f'Primos = {primos}\nTotal de divisões = {divisoes}')
+def teste_divisoes_restantes (numero_para_testar, lista_primos, divisoes):
+    for num in range (lista_primos [-1], (numero_para_testar | 2) + 1):
+        divisoes += 1
+        if numero_para_testar % num == 0:
+            primo = False
+            continue
+        else:
+            primo = True
+            break
+        
+    return primo, divisoes
+    
+
+for num in range (2, maximo + 1):   #pega cada numero que vai ser testado
+
+    teste_nao_primo, divisoes = divisivel_pelos_primos (num, lista_primos, divisoes)
+
+    if teste_nao_primo:
+        continue
+
+    else:
+        teste_primo, divisoes = teste_divisoes_restantes (num, lista_primos, divisoes)
+
+        if teste_primo:
+            lista_primos.append (num)
+
+        else:
+            continue
+
+print (f'Primos = {lista_primos}\nTotal de divisões = {divisoes}')
